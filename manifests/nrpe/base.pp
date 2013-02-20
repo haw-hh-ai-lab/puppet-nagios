bbd3b8862756ea3b76b5c335c1d72f7d02c3fc0b
Lutz Behnke <lutz.behnke@informatik.haw-hamburg.de>
2013-02-20 12:14:34 +0100

    turn of old default commands

--- manifests/nrpe/base.pp
+++ manifests/nrpe/base.pp
@@ -1,8 +1,5 @@
 class nagios::nrpe::base {
 
-    if $nagios_nrpe_cfgdir == '' { $nagios_nrpe_cfgdir = '/etc/nagios' }
-    if $processorcount == '' { $processorcount = 1 }
-    
     package { 	"nagios-nrpe-server": ensure => present;
 		    "nagios-plugins-basic": ensure => present;
 		    "libwww-perl": ensure => present;   # for check_apache
@@ -22,23 +19,24 @@ class nagios::nrpe::base {
 	    owner => root, group => 0, mode => 644;
     }
     
-    # default commands
+    # add some default commands
+    # FIXME: currently all commands in this file are commented out, as they use args.
     nagios::nrpe::command { "basic_nrpe":
-        source => [ "puppet:///modules/site-nagios/configs/nrpe/nrpe_commands.${fqdn}.cfg",
-                    "puppet:///modules/site-nagios/configs/nrpe/nrpe_commands.cfg",
-                    "puppet:///modules/nagios/nrpe/nrpe_commands.cfg" ],
+        source => 'puppet:///modules/nagios/nrpe/nrpe_commands.cfg',
     }
+    
+    # FIXME: this is far to general to be usefull.
     # the check for load should be customized for each server based on number
-    # of CPUs and the type of activity.
-    $warning_1_threshold = 7 * $processorcount
-    $warning_5_threshold = 6 * $processorcount
-    $warning_15_threshold = 5 * $processorcount
-    $critical_1_threshold = 10 * $processorcount
-    $critical_5_threshold = 9 * $processorcount
-    $critical_15_threshold = 8 * $processorcount
-    nagios::nrpe::command { "check_load":
-        command_line => "${nagios_plugin_dir}/check_load -w ${warning_1_threshold},${warning_5_threshold},${warning_15_threshold} -c ${critical_1_threshold},${critical_5_threshold},${critical_15_threshold}",
-    }
+    # of CPUs and the type of activity. Get number of processors from facter
+    #$warning_1_threshold = 7 * $::processorcount
+    #$warning_5_threshold = 6 * $::processorcount
+    #$warning_15_threshold = 5 * $::processorcount
+    #$critical_1_threshold = 10 * $::processorcount
+    #$critical_5_threshold = 9 * $::processorcount
+    #$critical_15_threshold = 8 * $::processorcount
+    #nagios::nrpe::command { "check_load":
+    #    command_line => "${nagios_plugin_dir}/check_load -w ${warning_1_threshold},${warning_5_threshold},${warning_15_threshold} -c ${critical_1_threshold},${critical_5_threshold},${critical_15_threshold}",
+    #}
 
     service { "nagios-nrpe-server":
 	    ensure    => running,
