bca28b37e4f9f5313de971c12aa1b0fc94f22a19
Lutz Behnke <lutz.behnke@informatik.haw-hamburg.de>
2013-09-26 15:49:07 +0200

    fix name of service

--- manifests/nrpe/ubuntu.pp
+++ manifests/nrpe/ubuntu.pp
@@ -4,6 +4,6 @@ class nagios::nrpe::ubuntu inherits nagios::nrpe::base {
     Package["nagios-nrpe-server"] { name => "nagios-nrpe-server" }
     Package["nagios-plugins-basic"] { name => "nagios-plugins" }
 
-    Service['nagios-nrpe-server'] { name => 'nrpe' }
+    Service['nagios-nrpe-server'] { name => 'nagios-nrpe-server' }
 
 }
