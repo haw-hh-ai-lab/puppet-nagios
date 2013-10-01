6aa5cd5721daf033ef5ffdb3353bcda5993cd35e
Lutz Behnke <lutz.behnke@informatik.haw-hamburg.de>
2013-10-01 11:18:44 +0200

    fix declaration for config files from package

--- manifests/ubuntu.pp
+++ manifests/ubuntu.pp
@@ -15,16 +15,30 @@ class nagios::ubuntu inherits nagios::base {
     File['nagios_htpasswd', 'nagios_cgi_cfg'] { group => 'www-data' }
 
     file { 'nagios_commands_cfg':
-            path   => "${nagios::defaults::vars::int_cfgdir}/commands.cfg",
-            ensure => present,
-            notify => Service['nagios'],
-            mode   => 0644, owner => root, group => root;
+	path   => "${nagios::defaults::vars::int_cfgdir}/commands.cfg",
+	ensure => present,
+	notify => Service['nagios'],
+	mode   => 0644, owner => root, group => root;
     }
 
     file { "${nagios::defaults::vars::int_cfgdir}/stylesheets":
         ensure => directory,
         purge => false,
         recurse => true,
+    }
+
+    file { 'nagios_generic_host_cfg':
+	path  => "${nagios::defaults::vars::int_cfgdir}/conf.d/generic-host_nagios2.cfg",
+	ensure => present,
+	notify => Service['nagios'],
+	mode   => 0644, owner => root, group => root
+    }
+
+    file { 'nagios_generic_service_cfg':
+        path  => "${nagios::defaults::vars::int_cfgdir}/conf.d/generic-service_nagios2.cfg",
+        ensure => present,
+        notify => Service['nagios'],
+        mode   => 0644, owner => root, group => root
     }
 
     if $nagios::allow_external_cmd {
