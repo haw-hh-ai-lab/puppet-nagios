eef748b4494c051abcb3a63a01e379d681c634e8
Lutz Behnke <lutz.behnke@informatik.haw-hamburg.de>
2013-10-09 16:02:09 +0200

    set path to cgi set in php config for Ubuntu

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,7 +5,8 @@ class nagios::params {
 	case $::operatingsystem {
 		'Ubuntu' : {
 			$cgi_dir = "/usr/lib/cgi-bin/nagios3/"
-			$web_dir = "/usr/share/nagios3/htdocs/"
+			$cgi_path = "/cgi-bin/nagios3/"
+                        $web_dir = "/usr/share/nagios3/htdocs/"
   		}
 		default: {
 			fail("running nagios monitoring server not supported on  operating system $::operatingsystem")
