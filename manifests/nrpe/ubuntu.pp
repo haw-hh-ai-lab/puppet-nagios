6fe3e137864729bbeaf81857f99b32ade8c479e0
Lutz Behnke <lutz.behnke@informatik.haw-hamburg.de>
2013-10-10 15:17:31 +0200

    get sufficient plugins for nrpe on ubuntu

--- manifests/nrpe/ubuntu.pp
+++ manifests/nrpe/ubuntu.pp
@@ -2,7 +2,7 @@ class nagios::nrpe::ubuntu inherits nagios::nrpe::base {
 
 
     Package["nagios-nrpe-server"] { name => "nagios-nrpe-server" }
-    Package["nagios-plugins-basic"] { name => "nagios-plugins" }
+    Package["nagios-plugins-basic"] { name => "nagios-plugins-basic" }
 
     Service['nagios-nrpe-server'] { name => 'nagios-nrpe-server' }
 
