83237b2c623d3ad219d592f7f3f76c0fa2ef0150
Lutz Behnke <lutz.behnke@informatik.haw-hamburg.de>
2014-04-02 09:08:41 +0200

    add facts

--- spec/classes/nagios_spec.rb
+++ spec/classes/nagios_spec.rb
@@ -6,6 +6,10 @@ require 'spec_helper'
 describe 'nagios' do
   let(:hiera_config) { 'spec/fixtures/hiera/hiera.yaml' }
   
+  let(:facts) { {:operatingsystem => 'Debian', 
+                 :operatingsystemrelease => 13.10,
+                 :ipaddress => '192.168.0.1'} }
+  
   context 'minimal setup' do
     let(:title) { 'Test NFS Mount' }
 
