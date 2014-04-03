7e36e0bcb0f795683146a1f2c14787b96b02538e
Lutz Behnke <lutz.behnke@informatik.haw-hamburg.de>
2014-04-03 08:59:07 +0200

    populate config for dependency module concat

--- spec/classes/nagios_spec.rb
+++ spec/classes/nagios_spec.rb
@@ -6,10 +6,16 @@ require 'spec_helper'
 describe 'nagios' do
   let(:hiera_config) { 'spec/fixtures/hiera/hiera.yaml' }
   
-  let(:facts) { {:domain => 'example.com',
-				 :fqdn => 'testnode.example.com',
-                 :hostname => 'testnode',
-                 :operatingsystem => 'Ubuntu',
+  concatdir            = '/var/lib/puppet/concat'
+    
+  let(:facts) { {
+                 # the module concat needs this. Normaly set by concat through pluginsync
+                 :concat_basedir         => concatdir,
+                   
+                 :domain                 => 'example.com',
+				 :fqdn                   => 'testnode.example.com',
+                 :hostname               => 'testnode',
+                 :operatingsystem        => 'Ubuntu',
                  :operatingsystemrelease => '13.10',
                  :osfamily => 'Debian',
                  :ipaddress => '192.168.0.1'} }
