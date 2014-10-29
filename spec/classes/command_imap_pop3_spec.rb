edf130ae83465ec771d90e950e7c7f1f7fdca341
Lutz Behnke <lutz.behnke@informatik.haw-hamburg.de>
2014-10-29 15:36:23 +0100

    twiddle twaddle

--- spec/classes/command_imap_pop3_spec.rb
+++ spec/classes/command_imap_pop3_spec.rb
@@ -6,11 +6,13 @@ require 'spec_helper'
 describe 'nagios::command::imap_pop3' do
   let(:hiera_config) { 'spec/fixtures/hiera/hiera.yaml' }
     
+    
   context 'set commands for default environment' do
+   
     
     it do
 
-      should contain_nagios_command('check_imap_ssl').that_requires('Package[nagios]')
+      should contain_nagios_command('check_imap_ssl')
       should contain_nagios_command('check_pop3').that_requires('Package[nagios]')
       should contain_nagios_command('check_pop3_ssl').that_requires('Package[nagios]')
       should contain_nagios_command('check_managesieve').that_requires('Package[nagios]')
